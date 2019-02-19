b767e56476713d75e91b5fe750d367dc77895577
Jan Bator <jan.bator@xing.com>
2019-02-19 11:49:16 +0700

    Move templates to text files.

--- lib/cowsay.rb
+++ lib/cowsay.rb
@@ -1,28 +1,24 @@
-require 'cowsay/version'
-require 'cowsay/character'
+require_relative './cowsay/version'
+require_relative './cowsay/character'
 
 module ::Cowsay
   module_function # all instance methods are available on the module (class) level
 
   def random_character
-    random_class = Character.const_get(character_classes[rand(character_classes.length)])
-    random_class.new
+    Character.new(character_classes[rand(character_classes.length)])
   end
 
   def character_classes
-    @character_classes ||= Character.constants.map { |c| c.to_sym } - [:Base, :Template]
+    @character_classes ||= Dir.children(''./lib/templates').map { |t| File.basename(t, '.txt') }
   end
 
-  def say(message, character)
-    character ||= 'cow'
-    if character == 'random'
+  def say(message, template_name)
+    if template_name == 'random'
       random_character.say(message)
+    elsif character_classes.include? template_name
+      Character.new(template_name).say(message)
     else
-      if character_classes.include? character.capitalize.to_sym
-        Character.const_get(character.capitalize).say(message)
-      else
-        puts "No cow file found for #{character}. Use the -l flag to see a list of available cow files."
-      end
+      puts "No cow file found for #{template_name}. Use the -l flag to see a list of available cow files."
     end
   end
 end
